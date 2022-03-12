b23af846e7258ca3eb7af3d87d824c00dc11526a
hashimcodes <hashem.10.hs@gmail.com>
2022-03-12 16:39:43 +0200

    add sounds

--- functions/pickaCard.py
+++ functions/pickaCard.py
@@ -1,12 +1,21 @@
 import random
+import time
+from pygame import mixer
+
+
 cards = ["Diamonds", "Spades", "Hearts", "Clubs"]
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
 
 def pickCard():
+    mixer.init()
+    mixer.music.load("data/records/card_shuffle.wav")
+    mixer.music.play()
+    while mixer.music.get_busy():
+        time.sleep(1)
     card = random.choices(cards)
     rank = random.choices(ranks)
-    return(f"The {rank} of {card}")
+    return(f"The {rank[0]} of {card[0]}")
 
 
 # print(pickCard())
--- functions/rollDie.py
+++ functions/rollDie.py
@@ -6,8 +6,11 @@ from pygame import mixer
 def rollDie():
     mixer.init()
     mixer.music.load(
-        "data/records/rollDie.mpeg")
+        "./data/records/rollDie.mpeg")
     mixer.music.play()
     while mixer.music.get_busy():  # wait for music to finish playing
         time.sleep(1)
     return 'it is ' + str(random.randint(1, 6))
+
+
+print(rollDie())
