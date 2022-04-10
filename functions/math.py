2705414fd6ec017543de9be3c2b4634b66770381
hashimcodes <hashem.10.hs@gmail.com>
2022-04-10 22:15:07 +0200

    Noah Fixed Math Function

--- functions/math.py
+++ functions/math.py
@@ -10,11 +10,11 @@ def calculate(text):
         result = int(text[0])+int(text[-1])
     elif text[1] == "-" or text[1] == "Subtract" or text[1] == "minus":
         result = int(text[0])-int(text[-1])
-    elif text[1] == "*" or text[1] == "multiplied" or text[1] == "multiply":
+    elif text[1] == "*" or text[1] == "multiplied" or text[1] == "multiply" or text[1] == "x":
         result = int(text[0])*int(text[-1])
     elif text[1] == "/" or text[1] == "divided":
         result = int(text[0])/int(text[-1])
-    elif text[1] == "**" or text[1] == "power":
+    elif text[1] == "**" or text[1] == "power" or text[1] == "-power":
         result = int(text[0])**int(text[-1])
 
     return str(result)
